289b5179c1cc2fe91e6f4ad9512afd7ed8283271
Nate <65456795+natealberti@users.noreply.github.com>
2024-11-14 13:57:37 -0700

    new hyperparameters

--- run.sh
+++ run.sh
@@ -9,8 +9,8 @@ PRETRAINED_WEIGHTS=/scratch/nralbert/CSE507/PEAC_backup/weights/last_swinb_patch
 OUTPUT_DIR=/scratch/nralbert/research/Ark/output/debug
 
 ~/.conda/envs/[ENVIRONMENT]/bin/python main_ark.py --data_set ChestXray14	\
---opt sgd --warmup-epochs 20  --lr 0.3     	\
---batch_size 200 --model swin_base --init peac  \
---pretrain_epochs 200  --test_epoch 1	\
+--opt adamw --lr 0.0005 --warmup-epochs 20     	\
+--batch_size 128 --model swin_base --init peac  \
+--pretrain_epochs 150  --test_epoch 5	\
 --pretrained_weights $PRETRAINED_WEIGHTS	\
 --momentum_teacher 0.9  --projector_features 1376
