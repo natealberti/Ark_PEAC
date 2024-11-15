876f035a9b305d98d8ace9482b9deca142f6639f
Peter Mousses <29151865+petermousses@users.noreply.github.com>
2024-11-15 09:01:25 -0700

    Improved run.sh

--- run.sh
+++ run.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
+ENVIRONMENT=Ark_PEAC
 module load mamba/latest
-source activate [ENVIRONMENT]
+source activate $ENVIRONMENT
 
 # PRETRAINED_WEIGHTS=/scratch/nralbert/research/checkpoints/swin_base_patch4_window7_224.pth             ### Swin-B ImageNet-1K weights
-PRETRAINED_WEIGHTS=/scratch/nralbert/CSE507/PEAC_backup/weights/last_swinb_patchsize32_imgsize448.pth    ### Swin-B PEAC weights (ss pretrained on NIH chest x-rays)
+PRETRAINED_WEIGHTS_NATE=/scratch/nralbert/CSE507/PEAC_backup/weights
+PRETRAINED_WEIGHTS_PETER=/scratch/pmousses/PEAC_weights
+
+### Swin-B PEAC weights (ss pretrained on NIH chest x-rays)
+WEIGHTS_FILE_NAME=last_swinb_patchsize32_imgsize448.pth
+PRETRAINED_WEIGHTS="$PRETRAINED_WEIGHTS_PETER/$WEIGHTS_FILE_NAME"
 
 OUTPUT_DIR=/scratch/nralbert/research/Ark/output/debug
 
-~/.conda/envs/[ENVIRONMENT]/bin/python main_ark.py --data_set ChestXray14	\
+~/.conda/envs/$ENVIRONMENT/bin/python main_ark.py --data_set ChestXray14	\
 --opt adamw --lr 0.0005 --warmup-epochs 20     	\
 --batch_size 128 --model swin_base --init peac  \
 --pretrain_epochs 150  --test_epoch 5	\
