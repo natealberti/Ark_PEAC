1e776844f780f1098cb6860d46783e73e984911c
Peter Mousses <29151865+petermousses@users.noreply.github.com>
2024-11-16 22:23:13 -0700

    Added sbatch options and optional installation from environment.yml

--- run.sh
+++ run.sh
@@ -1,7 +1,25 @@
 #!/bin/bash
 
-ENVIRONMENT=Ark_PEAC
+#SBATCH -N 1            # number of nodes
+#SBATCH -c 20           # number of cores 
+#SBATCH --mem=64G		# Memory in GiB
+#SBATCH -G a100:1       # number of gpus
+#SBATCH -t 1-00:00:00   # time in d-hh:mm:ss
+#SBATCH -p general      # partition 
+#SBATCH -q public       # QOS
+#SBATCH -o slurm.%j.out # file to save job's STDOUT (%j = JobId)
+#SBATCH -e slurm.%j.err # file to save job's STDERR (%j = JobId)
+#SBATCH --mail-type=ALL # Send an e-mail when a job starts, stops, or fails
+#SBATCH --mail-user="%u@asu.edu"
+#SBATCH --export=NONE   # Purge the job-submitting shell environment
+
+ENV_INSTALLED=${1:-true}
+ENVIRONMENT=${2:-"Ark_PEAC"}
 module load mamba/latest
+if [ "$ENV_INSTALLED" = false ]; then
+	mamba env create -f environment.yml
+	ENVIRONMENT=Ark_PEAC
+fi
 source activate $ENVIRONMENT
 
 # PRETRAINED_WEIGHTS=/scratch/nralbert/research/checkpoints/swin_base_patch4_window7_224.pth             ### Swin-B ImageNet-1K weights
