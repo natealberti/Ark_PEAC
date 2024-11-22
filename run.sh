8d50dd1f52e46cd1b604fb42db30bf71ed4b6347
Nate <65456795+natealberti@users.noreply.github.com>
2024-11-21 20:42:52 -0700

    update hyperparams

--- run.sh
+++ run.sh
@@ -25,7 +25,7 @@ fi
 source activate $ENVIRONMENT
 
 # PRETRAINED_WEIGHTS=/scratch/nralbert/research/checkpoints/swin_base_patch4_window7_224.pth             ### Swin-B ImageNet-1K weights
-PRETRAINED_WEIGHTS_NATE=/scratch/nralbert/CSE507/PEAC_backup/weights
+PRETRAINED_WEIGHTS_NATE=/scratch/nralbert/research/checkpoints
 PRETRAINED_WEIGHTS_PETER=/scratch/pmousses/PEAC_weights
 
 ### Swin-B PEAC weights (ss pretrained on NIH chest x-rays)
@@ -36,8 +36,7 @@ OUTPUT_DIR=/scratch/$USER/research/Ark/output/debug
 mkdir -p $OUTPUT_DIR
 
 ~/.conda/envs/$ENVIRONMENT/bin/python main_ark.py --data_set ChestMNIST	\
---opt adamw --lr 0.0005 --warmup-epochs 20     	\
---batch_size 128 --model swin_base --init peac  \
---pretrain_epochs 150  --test_epoch 5	\
---pretrained_weights $PRETRAINED_WEIGHTS	\
---momentum_teacher 0.9  --projector_features 1376
+--opt adamw --lr 1e-4  	\
+--batch_size 24 --model swin_base --pretrained_weights $PRETRAINED_WEIGHTS --init peac \
+--pretrain_epochs 300  --test_epoch 5 --sched cosine	\
+--momentum_teacher 0.9
